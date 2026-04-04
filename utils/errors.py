73813db91b80422dd4004b27f4e81902451ebb93
wellnaxx <highonosu@gmail.com>
2026-04-04 23:55:29 +0300

    refactor: clean up error classes in errors.py for user service

--- utils/errors.py
+++ utils/errors.py
@@ -1,45 +1,39 @@
 class DatabaseError(RuntimeError):
     """Raised when a database operation fails."""
 
-# class UserServiceError(Exception):
-#     """Base auth/service error."""
+
+class UserServiceError(Exception):
+    """Base auth/service error."""
 
 
-# class UsernameAlreadyExistsError(UserServiceError):
-#     pass
+class UsernameAlreadyExistsError(UserServiceError):
+    pass
 
 
-# class InvalidCredentialsError(UserServiceError):
-#     pass
+class EmailAlreadyExistsError(UserServiceError):
+    pass
 
 
-# class InvalidRefreshTokenError(UserServiceError):
-#     pass
+class InvalidCredentialsError(UserServiceError):
+    pass
 
 
-# class UserNotFoundError(UserServiceError):
-#     pass
+class IdenticalPasswordsError(UserServiceError):
+    pass
 
 
-# class IncorrectOldPasswordError(UserServiceError):
-#     pass
+class InvalidRefreshTokenError(UserServiceError):
+    pass
 
 
-# class MovieServiceError(Exception):
-#     """Base movie service error."""
+class UserNotFoundError(UserServiceError):
+    pass
 
+class UserCreationError(UserServiceError):
+    pass
 
-# class MovieNotFoundError(MovieServiceError):
-#     pass
+class IncorrectOldPasswordError(UserServiceError):
+    pass
 
-
-# class MovieCreationError(MovieServiceError):
-#     pass
-
-
-# class MovieDeleteError(MovieServiceError):
-#     pass
-
-
-# class EmptyMovieUpdateError(MovieServiceError):
-#     pass
+class UserDeleteError(UserServiceError):
+    pass
