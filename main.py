cc25e7e8c99d7245e98005fb3622cf30b353ea8a
wellnaxx <highonosu@gmail.com>
2026-04-06 18:16:35 +0300

    feat: include user_goals_router in the main application

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
+from routers.api.user_goals_router import user_goals_router
 from routers.api.users_router import users_router
 
 app = FastAPI()
 
 app.include_router(users_router)
+app.include_router(user_goals_router)
 
 
 @app.get("/")
