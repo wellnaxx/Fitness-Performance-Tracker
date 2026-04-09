962f8741e80f01255906fd49d02f8a57355915a1
wellnaxx <highonosu@gmail.com>
2026-04-09 10:59:33 +0300

    feat: add error classes for user goals and exercise operations

--- utils/errors.py
+++ utils/errors.py
@@ -14,6 +14,14 @@ class UserServiceError(ServiceError):
     """Base user-related service error."""
 
 
+class UserGoalsError(ServiceError):
+    """Base error for user goals operations."""
+
+
+class ExerciseServiceError(ServiceError):
+    """Base error for exercise-related operations."""
+
+
 class UsernameAlreadyExistsError(UserServiceError):
     """Raised when username already exists."""
 
@@ -50,9 +58,29 @@ class UserDeleteError(UserServiceError):
     """Raised when user deletion fails."""
 
 
-class UserGoalsError(ServiceError):
-    """Base error for user goals operations."""
-
-
 class UserGoalNotFoundError(UserGoalsError):
     """Raised when a goal is not found or does not belong to the user."""
+
+
+class UserGoalCreationError(UserGoalsError):
+    """Raised when creating a user goal fails."""
+
+
+class ExerciseNotFoundError(ExerciseServiceError):
+    """Raised when an exercise is not found."""
+
+
+class ExerciseCreationError(ExerciseServiceError):
+    """Raised when creating an exercise fails."""
+
+
+class ExerciseNameAlreadyExistsError(ExerciseServiceError):
+    """Raised when an exercise with the same name already exists for the user."""
+
+
+class ExerciseUpdateError(ExerciseServiceError):
+    """Raised when updating an exercise fails."""
+
+
+class ExerciseDeleteError(ExerciseServiceError):
+    """Raised when deleting an exercise fails."""
